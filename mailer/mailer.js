61c26336e3af8b874647fb32d86a3d6c4fb63646
darwme <darw.mendz@gmail.com>
2023-12-30 02:56:48 -0500

    new dotenv api mailer transporter added

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,14 +1,34 @@
 const nodemailer = require('nodemailer');
+const mailTrapTransport = require('mailtrap');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'leo.west@ethereal.email',
-        pass: 'XGjK2vckr5nQUVv4ts'
+let mailConfig;
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        auth: {
+            api_key: process.env.MAILTRAP_API_KEY,
+        }
+    }
+    mailConfig = mailTrapTransport(options);
+} else {
+    if (process.env.NODE_ENV === 'staging') {
+        console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.MAILTRAP_API_KEY,
+            }
+        }
+        mailConfig = mailTrapTransport(options);
+
+    } else {
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'leo.west@ethereal.email',
+                pass: 'XGjK2vckr5nQUVv4ts'
+            }
+        }
     }
 }
 
-
-
-module.exports = nodemailer.createTransport(mailConfig);
+    module.exports = nodemailer.createTransport(mailConfig);
