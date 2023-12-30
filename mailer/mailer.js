69a79eb040cf84720897b08c1acd54b4c5dbd7f7
darwme <darw.mendz@gmail.com>
2023-12-30 04:09:24 -0500

    possible make it with the mailer.js

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,29 +1,50 @@
-const Nodemailer = require("nodemailer");
-const MailtrapTransport = require("mailtrap");
+const nodemailer = require('nodemailer');
+const mailgunTransport = require('nodemailer-mailgun-transport');
 
+let transporter;
 
-
-const MAILTRAP_API_KEY = process.env.MAILTRAP_API_KEY || "";
-
-let mailConfig;
-
-if (MAILTRAP_API_KEY && process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    const options = {
-        auth: {
-            api_key: MAILTRAP_API_KEY,
-        },
-    };
-    mailConfig = Nodemailer.createTransport(MailtrapTransport({ options }));
+if (process.env.NODE_ENV === 'production') {
+  transporter = nodemailer.createTransport(mailgunTransport({
+    auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+    },
+  }));
 } else {
+  transporter  = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_CONTRASENA,
+    }
+}
+}
+
+module.exports = transporter;
+
+
+
+
+/*
+
+if (process.env.NODE_ENV === 'production') {
+    // Aquí puedes agregar configuración específica para producción
     mailConfig = {
+        auth: {
+            api_key: process.env.SENDGRID_API_KEY,
+        },
+        // Otros ajustes específicos de producción...
+    };
+}else{
+    const mailConfig = {
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: 'leo.west@ethereal.email',
-            pass: 'XGjK2vckr5nQUVv4ts',
+            user: process.env.MAILER_USER,
+            pass: process.env.MAILER_CONTRASENA,
         }
-    };
+    }
 }
 
-
-module.exports = Nodemailer.createTransport(mailConfig);
+module.exports = nodemailer.createTransport(mailConfig);
+*/
