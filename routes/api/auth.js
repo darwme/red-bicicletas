e48fde05afee65b01115c42e38f46d8565ede18a
darwme <darw.mendz@gmail.com>
2023-12-31 18:52:29 -0500

    commist new

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const authControllerAPI = require('../../controllers/api/authControllerAPI');
 const passport = require('passport');
 
-router.get('authenticate', authControllerAPI.authenticate);
-router.post('forgotPassword', authControllerAPI.forgotPassword);
+router.get('/authenticate', authControllerAPI.authenticate);
+router.post('/forgotPassword', authControllerAPI.forgotPassword);
 router.post('/facebook_token', passport.authenticate('facebook-token'), authControllerAPI.authFacebookToken);
 
 module.exports = router;
